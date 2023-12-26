633a426b7805912d70c08e12c28a87c4a09381ac
ezi-s-u <ljsgoo0617@gmail.com>
2023-12-26 17:19:52 +0900

    [refactor] 로그인, 회원가입 암호화 (랜덤화 전)

--- user-api/usersRouter.js
+++ user-api/usersRouter.js
@@ -11,23 +11,24 @@ const db = mysql.createConnection({
   database: 'livingalone',
 });
 
-router.post('/', (req, res) => {
-  // 사용자 정보 저장 API
-  const { user_id, user_name, user_password, user_email } = req.body;
+// router.post('/', (req, res) => {
+//   // 사용자 정보 저장 API
+//   const { user_id, user_name, user_password = crypto.createHash('sha512').update(user_password).digest('base64'), user_email } = req.body;
 
-  const query = 'INSERT INTO users (user_id, user_name, user_password, user_email) VALUES (?, ?, ?, ?)';
-  db.query(query, [user_id, user_name, user_password, user_email], (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
+//   const query = 'INSERT INTO users (user_id, user_name, user_password, user_email) VALUES (?, ?, ?, ?)';
+//   db.query(query, [user_id, user_name, user_password, user_email], (err, results) => {
+//     if (err) {
+//       return res.status(500).json({ error: err.message });
+//     }
 
-    res.json({ message: '사용자 정보가 성공적으로 저장되었습니다.' });
-  });
-});
+//     res.json({ message: '사용자 정보가 성공적으로 저장되었습니다.' });
+//   });
+// });
 
 router.post('/signup', (req, res) => {
   // 회원가입 중복 확인 API
-  const { user_id, user_name, user_password = crypto.createHash('sha512').update(user_password).digest('base64'), user_email } = req.body;
+  let { user_id, user_name, user_password, user_email } = req.body;
+  user_password = crypto.createHash('sha512').update(user_password).digest('base64');
 
   const checkQuery = 'SELECT * FROM users WHERE user_id = ? OR user_email = ?';
   db.query(checkQuery, [user_id, user_email], (err, results) => {
@@ -38,9 +39,6 @@ router.post('/signup', (req, res) => {
     if (results.length > 0) {
       res.status(400).json({ error: '이미 가입된 사용자입니다.' });
     } else {
-      // 사용자 비밀번호 암호화
-      const crypto_user_password = crypto.createHash('sha512').update(user_password).digest('base64');
-
       const insertQuery = 'INSERT INTO users (user_id, user_name, user_password, user_email) VALUES (?, ?, ?, ?)';
       db.query(insertQuery, [user_id, user_name, user_password, user_email], (err, results) => {
         if (err) {
